d3e6e267e9d6353cf859e99e839b1d7694f24c87
Rkebede <rutht.kebede@gmail.com>
2021-06-17 23:06:33 -0500

    creates fetch and renders data

--- src/Results.js
+++ src/Results.js
@@ -5,7 +5,26 @@ const Results = ({ cardPairs }) => {
   let [bestCards, setBestCards] = useState([])
   let [points, setPoints] = useState(0)
 
-  useEffect(findBestCards, [cardPairs])
+
+  useEffect(() => {
+    const getBestCards = () => {
+      fetch('http://localhost:4567/besthand', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({cards: cardPairs})
+      }).then(response => {
+        console.log(response)
+        return response.json();
+      }).then(data => {
+        setBestCards(data.hand)
+        setPoints(data.points)
+      })
+    }
+    getBestCards()
+  },[cardPairs])
 
   return (
     <div style={{ paddingTop:"5rem", paddingLeft:"30rem", paddingRight:"10rem"}} >
